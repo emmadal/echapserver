09f79cf34b86d834435094e229a9948b1fddf63b
emmadal <emmanueldalougou@gmail.com>
2024-03-03 07:10:04 +0000

    add user verification to delete and update category

---
+++ models/category.go
@@ -0,0 +1,80 @@
+package models
+
+import "oblackserver/db"
+
+// GetAllCategories return the list of categories
+func GetAllCategories() ([]Category, error) {
+	rows, err := db.DB.Query("SELECT id, title FROM category ORDER BY title")
+	defer rows.Close()
+	if err != nil {
+		return nil, err
+	}
+	var categories []Category
+	for rows.Next() {
+		var cat Category
+		err := rows.Scan(&cat.ID, &cat.Title)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, cat)
+	}
+	return categories, nil
+}
+
+// CreateCategory create category
+func CreateCategory(category Category) error {
+	query := `INSERT INTO category(title, user_id) VALUES (?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	result, err := stmt.Exec(category.Title, category.UserID)
+	if err != nil {
+		return err
+	}
+	
+	id, err := result.LastInsertId()
+	category.ID = id
+
+	return err
+}
+
+// GetCategoryByID get a category
+func GetCategoryByID(categoryID int64) (*Category, error) {
+	query := `SELECT id, title, user_id FROM category WHERE ID = ?`
+	rows := db.DB.QueryRow(query, categoryID)
+	var category Category
+
+	err := rows.Scan(&category.ID, &category.Title, &category.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+// UpdateCategory update a category
+func UpdateCategory(category Category) error {
+	query := `UPDATE category SET title = ? WHERE id = ?`
+	stmt, err := db.DB.Prepare(query)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(category.Title, category.ID)
+	return err
+}
+
+// DeleteCategory delete a category
+func DeleteCategory(categoryID int64) error {
+	query := `DELETE FROM category WHERE ID = ?`
+	stmt, err := db.DB.Prepare(query)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(categoryID)
+	return err
+}
---
+++ routes/category.go
@@ -0,0 +1,205 @@
+package routes
+
+import (
+	"net/http"
+	"oblackserver/helpers"
+	"oblackserver/models"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getCategories(context *gin.Context) {
+	categories, err := models.GetAllCategories()
+	if err != nil {
+		context.SecureJSON(http.StatusNotFound, gin.H{
+			"message": "Data not found",
+			"success": false,
+		})
+		return
+	}
+	context.SecureJSON(http.StatusOK, gin.H{
+		"data":  categories,
+		"success": true,
+	})
+}
+
+func createCategory(context *gin.Context) {	
+	userID := context.GetInt64("userID")
+
+	var category models.Category
+	category.UserID = userID
+
+	err := context.ShouldBindJSON(&category)
+	if err != nil {
+		context.SecureJSON(http.StatusBadRequest, gin.H{"message": "Bad request", "success": false})
+		return
+	}
+
+	
+	err = models.CreateCategory(category)
+	if err != nil {
+		context.SecureJSON(http.StatusInternalServerError, gin.H{
+			"message": "Unable to create data", 
+			"success": false,
+		})
+		return
+	}
+
+	context.SecureJSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Article created successfully",
+	})
+
+}
+
+func getCategoryByID(context *gin.Context) {
+	id := context.Param("id")
+	categoryID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse category ID",
+			"success": false,
+		})
+		return
+	}
+	category, err := models.GetCategoryByID(categoryID)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"message": "Unable to fetch category data", 
+			"success": false,
+		})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data": category,
+	})
+}
+
+func updateCategory(context *gin.Context) {
+	// convert id parameter into int64
+	id := context.Param("id")
+
+	// get the userID in the context
+	userID := context.GetInt64("userID")
+
+	categoryID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		context.SecureJSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse category ID",
+			"success": false,
+		})
+		return
+	}
+
+	// verify if the category exist in the database
+	category, err :=  models.GetCategoryByID(categoryID)
+	if err != nil {
+		context.SecureJSON(http.StatusNotFound, gin.H{
+			"message": "Unable to find this data", 
+			"success": false,
+		})
+		return
+	}
+
+	// We verify if it's the category owner before to update
+	if userID != category.UserID {
+		context.SecureJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update it", "success": false})
+		return
+	}
+
+	// send the new  data to be updated on the database
+	var newData models.Category
+	newData.UserID = userID
+	err = context.ShouldBindJSON(&newData)
+	if err != nil {
+		context.SecureJSON(http.StatusBadRequest, gin.H{"message": "Bad request", "success": false})
+		return
+	}
+
+	// Update the Category with the new Data
+	newData.ID = categoryID
+	err = models.UpdateCategory(newData)
+	if err != nil {
+		context.SecureJSON(http.StatusInternalServerError, gin.H{
+			"message": "Unable to update category data", 
+			"success": false,
+		})
+		return
+	}
+	context.SecureJSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Category updated",
+	})
+}
+
+func deleteCategory(context *gin.Context) {
+	// convert id parameter into int64
+	id := context.Param("id")
+
+	// get the userID in the context
+	userID := context.GetInt64("userID")
+	
+	categoryID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		context.SecureJSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse category ID",
+			"success": false,
+		})
+		return
+	}
+
+	// verify is the category exist in the database
+	category, err :=  models.GetCategoryByID(categoryID)
+	if err != nil {
+		context.SecureJSON(http.StatusNotFound, gin.H{
+			"message": "Unable to find this data", 
+			"success": false,
+		})
+		return
+	}
+
+	// We verify if it's the category owner before to update
+	if userID != category.UserID {
+		context.SecureJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete it", "success": false})
+		return
+	}
+
+	// delete the category
+	err = models.DeleteCategory(categoryID)
+	if err != nil {
+		context.SecureJSON(http.StatusInternalServerError, gin.H{
+			"message": "Unable to delete category", 
+			"success": false,
+		})
+		return
+	}
+
+	context.SecureJSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Category deleted",
+	})
+}
+
+func uploadImage(context *gin.Context) {
+	header, err := context.FormFile("file") // return the first file for the provided form key.
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error(), "success": false})
+		return
+	}
+	url, err := helpers.UploadHelper(header) 
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to open uploaded file", 
+			"success": false,
+		})
+		return;
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"message": "file uploaded", 
+		"success": true,
+		"data": url,
+	})
+}
